92ea76426ce9949ac49cc90b17455b0a90c73bfe
Shumon Khan <codewithshumon@gmail.com>
2025-10-06 00:53:16 +0600

    app initialize

---
+++ app/(page)/layouts/AppLayout.jsx
@@ -0,0 +1,11 @@
+import { SideBar } from './SideBar'
+
+const AppLayout = () => {
+  return (
+    <div>
+      <SideBar />
+    </div>
+  )
+}
+
+export default AppLayout
---
+++ app/(page)/layouts/SideBar.jsx
@@ -0,0 +1,7 @@
+'use client'
+
+export const SideBar = () => {
+  return (
+    <div>SideBar</div>
+  )
+}
